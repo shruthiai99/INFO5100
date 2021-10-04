e13ec4ae7e2e06cbe3afb68ced5460bc6d3168bd
Shruthi A <shruthiai99@gmail.com>
2021-10-04 07:01:22 -0700

    Sum solution

---
+++ Assignment2/Sum/src/neu/psa/Sum.java
@@ -0,0 +1,25 @@
+package neu.psa;
+
+public class Sum {
+
+    public int add(int a, int b){
+        return a + b;
+    }
+
+    public int add(int a, int b, int c){
+        return a + b + c;
+    }
+
+    public double add(double a, double b){
+        return a + b;
+    }
+
+    public double add(double a, int b){
+        return a + b;
+    }
+
+    public double add(int a, double b){
+        return a + b;
+    }
+
+}
