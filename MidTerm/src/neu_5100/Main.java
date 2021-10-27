e9ecea90b6ef17a3195cb0778b3cd8103164652f
shruthiai99 <90571560+shruthiai99@users.noreply.github.com>
2021-10-26 20:39:47 -0700

    Updated solution4

--- MidTerm/src/neu_5100/Main.java
+++ MidTerm/src/neu_5100/Main.java
@@ -1,9 +1,7 @@
 package neu_5100;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.sql.Array;
+import java.util.*;
 
 public class Main {
 
@@ -14,7 +12,7 @@ public class Main {
         int[] nums = {1, 2, 3, 4, 5};
         System.out.println(sumOfRepeated(nums));
 
-        System.out.println(nonRepeatingSubString("abcabcbb"));
+        System.out.println(nonRepeated("abcabcbb"));
     }
 
     //Question 3
@@ -41,35 +39,63 @@ public class Main {
         return result;
     }
 
-    public static String nonRepeatingSubString(String string) {
-        Set<Character> charSet = new HashSet<>();
+//    public static String nonRepeatingSubString(String string) {
+//        Set<Character> charSet = new HashSet<>();
+//        StringBuilder sb = new StringBuilder();
+//        sb.append("");
+//        for (char letter : string.toCharArray()) {
+//            charSet.add(letter);
+//        }
+//
+//        for (int i = 0; i < string.length() - charSet.size() + 1; i++) {
+//            String subString = string.substring(i, i + charSet.size());
+//            Set<Character> subSet = new HashSet<>();
+//            for (char letter : subString.toCharArray()) {
+//                subSet.add(letter);
+//            }
+//            if (subSet.size() > sb.length()) {
+//                for (char letter : subSet) {
+//                    sb.append(letter);
+//                }
+//            }
+//
+//            if (subSet.size() == charSet.size()) {
+//                for (char letter : subSet) {
+//                    sb.append(letter);
+//                    return sb.toString();
+//                }
+//            }
+//        }
+//
+//        return sb.toString();
+//    }
+//Question 4
+    public static String nonRepeated(String string){
         StringBuilder sb = new StringBuilder();
-        sb.append("");
-        for (char letter : string.toCharArray()) {
-            charSet.add(letter);
-        }
+        StringBuilder sb2 = new StringBuilder();
 
-        for (int i = 0; i < string.length() - charSet.size() + 1; i++) {
-            String subString = string.substring(i, i + charSet.size());
-            Set<Character> subSet = new HashSet<>();
-            for (char letter : subString.toCharArray()) {
-                subSet.add(letter);
+        Set<Character> set = new HashSet<>();
+        char[] chars = string.toCharArray();
+        int i = 0;
+        List<String> list = new ArrayList<>();
+        String result = "";
+        while(i < chars.length){
+            if(!set.contains(chars[i])){
+                set.add(chars[i]);
+                sb.append(chars[i]);
+                i++;
             }
-            if (subSet.size() > sb.length()) {
-                for (char letter : subSet) {
-                    sb.append(letter);
+            else{
+                if(sb.length() > result.length()){
+                    result = sb.toString();
                 }
+                sb = new StringBuilder();
+                set.clear();
             }
 
-            if (subSet.size() == charSet.size()) {
-                System.out.println("here");
-                for (char letter : subSet) {
-                    sb.append(letter);
-                    return sb.toString();
-                }
-            }
+
+
         }
-
-        return sb.toString();
+        return result;
     }
 }
