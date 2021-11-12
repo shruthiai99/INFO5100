5ddde05bfe1304bd8f3d16ae5360be92d5d5bc33
Shruthi A <shruthiai99@gmail.com>
2021-11-12 13:39:39 -0800

    Traffic light solution

---
+++ Assignment4/TrafficLight/src/neu_psa/TrafficLight.java
@@ -0,0 +1,26 @@
+package neu_psa;
+
+public class TrafficLight {
+
+
+        private int canCross = 1;
+
+        public TrafficLight() {}
+
+        public void carArrived(
+                int carId,           // ID of the car
+                int roadId,          // ID of the road the car travels on. Can be 1 (road A) or 2 (road B)
+                int direction,       // Direction of the car
+                Runnable turnGreen,  // Use turnGreen.run() to turn light to green on current road
+                Runnable crossCar   // Use crossCar.run() to make car cross the intersection
+        ) {
+            synchronized (this) {
+                if (canCross != roadId) {
+                    canCross = roadId;
+                    turnGreen.run();
+                }
+                crossCar.run();
+            }
+        }
+    }
+
